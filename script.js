c3a4f41e83a5a2f34406f8689d845c8bd40e6bcc
Muhammad-Bilal09 <156311521+Muhammad-Bilal09@users.noreply.github.com>
2024-05-14 10:21:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function askingName() {
+    let name = prompt('Enter your name'); 
+    document.getElementById('output').innerHTML=name  
+}
+function showresult(){
+    document.getElementById('output').innerHTML=" "
+}
+function condition(){
+    var marks=prompt('enter your marks');
+    document.getElementById('output').innerHTML=marks  
+     if(marks>80)
+    System.outptintln('A+')
+    
+    elseif(marks>70);
+    System.outptintln('A+')
+
+ifelse(marks>60);
+System.outptintln('A+')
+ifelse(marks>50);
+System.outptintln('A+')
+ifelse(marks>40);
+System.outptintln('A+')
+}
+function switchb(){
+    var int=prompt('enter your number')
+    document.getElementById('output').innerHTML=int  
+}
